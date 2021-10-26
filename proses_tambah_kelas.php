0d030fa6c075192ed56dd10ed36844532b51a1dd
20510001 <Febrianismail22@gmail.com>
2021-10-26 14:04:22 +0700

    tugas insert data

---
+++ proses_tambah_kelas.php
@@ -0,0 +1,27 @@
+<?php
+if($_POST){
+    $nama_jurusan=$_POST['nama_jurusan'];
+    $angkatan=$_POST['angkatan'];
+    if(empty($nama_jurusan)){
+        echo "<script>alert('Nama jurusan tidak boleh kosong');
+        location.href='tambah_kelas.php';</script>";
+
+    }elseif(empty($angkatan)){
+        echo "<script>alert('Angkatan tidak boleh kosong');
+        location.href='tambah_kelas.php';</script>";
+
+    } else {
+        include "koneksi.php";
+        $insert=mysqli_query($conn,"insert into jurusan (nama_jurusan, angkatan) value ('".
+        $nama_jurusan."','".$angkatan."')");
+        if($insert){
+            echo "<script>alert('Sukses menambahakan data
+            jurusan');location.href='tambah_kelas.php';</script>";
+        
+        } else {
+            echo "<script>alert('Gagal menambahakan data jurusan')
+            ;location.href='tambah_kelas.php';</script>";
+        }
+    }
+}
+?>
